d4256e9cce6e005ede63d0f005e37583a140f3cd
RuslanKarabalin <160013969+RuslanKarabalin@users.noreply.github.com>
2025-12-18 21:28:01 +0300

    feature/setvice (#2)

--- src/main/java/ru/chernyshoff/dddjava/domain/Order.kt
+++ src/main/java/ru/chernyshoff/dddjava/domain/Order.kt
@@ -1,6 +1,9 @@
 package ru.chernyshoff.dddjava.domain
 
+import java.util.UUID
+
 data class Order(
+    val id: UUID,
     val cactuses: List<Cactus>,
     val fertilizers: List<Fertilizer>,
     val totalAmount: Double,
